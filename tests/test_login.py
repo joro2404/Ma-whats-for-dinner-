84f68b8a074f5baae6571af92b7147f09f9198a6
Veselin Angelov <veskbg@gmail.com>
2020-03-22 19:36:05 +0200

    testing test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,22 +1,20 @@
-from app import create_app
+# from app import create_app
 import unittest
 import pytest
 
+from tests.base import BaseTestCase
 
-app = create_app()
-
-
-class FlaskTestCase(unittest.TestCase):
+class FlaskTestCase(BaseTestCase):
 
     def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
+        # tester = app.test_client(self)
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
 
     def test_login_page_loads(self):
-        tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        # tester = app.test_client(self)
+        response = self.client.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
